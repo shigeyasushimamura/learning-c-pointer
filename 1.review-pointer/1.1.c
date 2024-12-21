e7d2c507cc23e5aa61118653e677101752c6d7f1
shigeyasushimamura <sgys.mtk@gmail.com>
2024-12-21 10:45:56 +0900

    👿 ポインタ変数とアドレス演算子と間接演算子の検証

--- 1.review-pointer/1.1.c
+++ 1.review-pointer/1.1.c
@@ -2,7 +2,16 @@
 volatile int i = 123;
 
 int main(void){
-    i = i + 456;
-    printf("%d\n",i);
+    int num = 123;
+    int *pi = &num;
+    printf("num:%d\n",num);
+    printf("&num:%p\n",&num);
+    printf("*&pi:%p\n",&pi);
+    // *は間接演算子：ポインタ変数が指している先の値を取得する
+    printf("*pi:%d\n",*pi);
+    // int型へのポインタpiが指している値を変更する
+    *pi = 321;
+    printf("Address of num:%p Value: %d\n",&num,num);
+
     return(0);
 }
