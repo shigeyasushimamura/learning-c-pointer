751488272208a2aa5a3264a0388819d30469ef1e
shigeyasushimamura <sgys.mtk@gmail.com>
2024-12-25 18:35:22 +0900

    literal-array

--- 4.pointer-and-array/4.9.c
+++ 4.pointer-and-array/4.9.c
@@ -13,7 +13,24 @@ int main(void){
         matrix[i] = (int*)malloc(sizeof(int)*columns);
     }
 
+
+    // 連続メモリ割り当て パターン1
+    int **matrix2 = (int **)malloc(sizeof(int) * rows);
+    matrix2[0] = (int*)malloc(rows * columns * sizeof(int));
+    for(int i=0;i<rows;i++){
+        matrix2[i] = matrix[0] + i * columns;
+    }
    
+   // 連続メモリ割り当て パターン2
+   int *matrix3 = (int*)malloc(sizeof(int) * rows * columns);
+
+   for(int i=0;i<rows;i++){
+    for(int j=0;j<columns;j++){
+        printf("i:%d j:%d\n",i,j);
+        *(matrix3 + i * rows + j) = i * j;
+        printf("%d\n",*(matrix3 + i * rows + j));
+    }
+   }
 
 
     return 0;
